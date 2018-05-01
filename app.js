ab23986f1b204a1c6101ffeb18a68e12b86e4540
jaymccormick <jaymccormi@gmail.com>
2018-05-01 10:19:51 -0400

    selecting by tag name

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // selections
 
-const heading = document.getElementById('myHeading');
+// single element that can be accessed directly
 const myButton = document.getElementById('myButton');
 const textInput = document.getElementById('changeColor');
+
+// selecting by tag: multiple elements returned as an array
+const heading = document.getElementsByTagName('h1')[0];
+const myParagraph = document.getElementsByTagName('p');
+const myList = document.getElementsByTagName('li');
 
 heading.addEventListener('click', () => {
   heading.style.color = 'grey';
@@ -11,3 +16,9 @@ myButton.addEventListener('click', () => {
   heading.style.color = textInput.value;
   textInput.value = '';
 });
+
+for (let i = 0; i < myList.length; i++ ){
+  myList[i].addEventListener('click', () => {
+    myList[i].style.color = "#4caf50";
+  })
+}
