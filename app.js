f7c55e695b43b9eccc561be43db27041bbd9148c
jaymccormick <jaymccormi@gmail.com>
2018-05-02 20:28:32 -0400

    fix li colors, use querySelectorAll

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const textInput = document.getElementById('changeColor');
 // selecting by tag or class: multiple elements returned as an array
 const heading = document.getElementsByTagName('h1')[0];
 const myParagraph = document.getElementsByTagName('p');
-const myList = document.getElementsByTagName('li');
+const myList = document.querySelectorAll('li');
 const redThing = document.getElementsByClassName('red-thing');
+const purpleThing = document.querySelectorAll('.purple-thing');
 
 heading.addEventListener('click', () => {
   heading.style.color = 'grey';
@@ -21,10 +22,9 @@ myButton.addEventListener('click', () => {
 for (let i = 0; i < myList.length; i++ ){
 
   myList[i].addEventListener('click', () => {
-    if (myList[i].className === "error-not-purple"){
-    myList[i].style.color = "red";
-    } else {
-      myList[i].style.color = "#4caf50";
+    if (myList[i].className === "purple-thing"){
+      myList[i].style.color = "purple";
+      myList[i].style.fontWeight = 'bold';
     }
   });
 }
